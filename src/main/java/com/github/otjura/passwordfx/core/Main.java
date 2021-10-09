410f5842169a4f47ca6a4fa3bf1dbf5d663622f4
Otso Rajala <ojrajala@gmail.com>
2021-10-09 14:53:24 +0300

    Change spaces to tabs. Tab size 8.

--- src/main/java/com/github/otjura/passwordfx/core/Main.java
+++ src/main/java/com/github/otjura/passwordfx/core/Main.java
@@ -15,28 +15,28 @@ import java.util.Objects;
 public class Main extends Application
 {
 
-    public static void main(String[] args)
-    {
-        launch(args);
-    }
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
 
-    @Override
-    public void start(Stage primaryStage)
-    {
-        try
-        {
-            Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(
-                    getClass().getClassLoader().getResource("MainWindow.fxml"))));
-            scene.getStylesheets().add("style.css");
+	@Override
+	public void start(Stage primaryStage)
+	{
+		try
+		{
+			Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(
+				getClass().getClassLoader().getResource("MainWindow.fxml"))));
+			scene.getStylesheets().add("style.css");
 
-            primaryStage.setTitle("PasswordFX");
-            primaryStage.getIcons().add(new Image("icon.png"));
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        }
-        catch (NullPointerException | IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
+			primaryStage.setTitle("PasswordFX");
+			primaryStage.getIcons().add(new Image("icon.png"));
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+		catch (NullPointerException | IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/com/github/otjura/passwordfx/gui/MainWindowController.java
+++ src/main/java/com/github/otjura/passwordfx/gui/MainWindowController.java
@@ -14,39 +14,35 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 public class MainWindowController implements Initializable
 {
-    @FXML
-    private Label mainLabel;
-    @FXML
-    private TextField resultTextField;
-    @FXML
-    private Button alphanumButton;
-    @FXML
-    private Button anycharButton;
+	@FXML
+	private Label mainLabel;
+	@FXML
+	private TextField resultTextField;
+	@FXML
+	private Button alphanumButton;
+	@FXML
+	private Button anycharButton;
 
-    private int passwordLength;
+	private int passwordLength;
 
-    private void generateAlphanumericPassword()
-    {
-        resultTextField.setText(RandomStringUtils.randomAlphanumeric(passwordLength));
-        resultTextField.requestFocus();
-    }
+	private void generateAlphanumericPassword()
+	{
+		resultTextField.setText(RandomStringUtils.randomAlphanumeric(passwordLength));
+		resultTextField.requestFocus();
+	}
 
-    private void generateCharacterPassword()
-    {
-        resultTextField.setText(RandomStringUtils.randomGraph(passwordLength));
-        resultTextField.requestFocus();
-    }
+	private void generateCharacterPassword()
+	{
+		resultTextField.setText(RandomStringUtils.randomGraph(passwordLength));
+		resultTextField.requestFocus();
+	}
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources)
-    {
-        passwordLength = 20;
-
-        resultTextField.setEditable(false);
-
-        alphanumButton.setOnAction(e -> generateAlphanumericPassword());
-
-        anycharButton.setOnAction(e -> generateCharacterPassword());
-
-    }
+	@Override
+	public void initialize(URL location, ResourceBundle resources)
+	{
+		passwordLength = 20;
+		resultTextField.setEditable(false);
+		alphanumButton.setOnAction(e -> generateAlphanumericPassword());
+		anycharButton.setOnAction(e -> generateCharacterPassword());
+	}
 }
